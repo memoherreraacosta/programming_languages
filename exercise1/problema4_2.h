8da583160b793d4b658746ecb139b6182a8278f5
Guillermo Herrera-Acosta <a01400835@itesm.mx>
2020-02-11 16:58:34 -0600

    Exercise 1 files added
    Signed-off-by: Guillermo Herrera-Acosta <a01400835@itesm.mx>

---
+++ exercise1/problema4_2.h
@@ -0,0 +1,12 @@
+int the_biggest(int x, int y){
+    if(x > y)
+        return x;
+    else
+        return y;
+}
+int the_smallest(int x, int y){
+    if(x < y)
+        return x;
+    else
+        return y;
+}
